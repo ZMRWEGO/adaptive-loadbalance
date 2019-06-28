1d088c9d17a6007047c40e02422631645b06a1e8
赵春林 <946599275@qq.com>
2019-06-28 18:00:05 +0800

    first test

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -1,5 +1,6 @@
 package com.aliware.tianchi;
 
+import java.util.Arrays;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.rpc.Invocation;
 import org.apache.dubbo.rpc.Invoker;
@@ -12,15 +13,16 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * @author daofeng.xjf
  *
- * 负载均衡扩展接口
- * 必选接口，核心接口
- * 此类可以修改实现，不可以移动类或者修改包名
- * 选手需要基于此类实现自己的负载均衡算法
+ * 负载均衡扩展接口 必选接口，核心接口 此类可以修改实现，不可以移动类或者修改包名 选手需要基于此类实现自己的负载均衡算法
  */
 public class UserLoadBalance implements LoadBalance {
 
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
+        //数组中存储的分别是[interface com.aliware.tianchi.HashInterface -> dubbo://provider-small:20880/com.aliware.tianchi.HashInterface?async=true&heartbeat=0&loadbalance=user&reconnect=false,
+        // interface com.aliware.tianchi.HashInterface -> dubbo://provider-medium:20870/com.aliware.tianchi.HashInterface?async=true&heartbeat=0&loadbalance=user&reconnect=false,
+        // interface com.aliware.tianchi.HashInterface -> dubbo://provider-large:20890/com.aliware.tianchi.HashInterface?async=true&heartbeat=0&loadbalance=user&reconnect=false]
+        // System.out.println("ZCL-DEBUG"+Arrays.toString(invokers.toArray()));
+        return invokers.get(0);
     }
 }
