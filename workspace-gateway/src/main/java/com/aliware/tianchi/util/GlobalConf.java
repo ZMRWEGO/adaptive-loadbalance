c66f92671517b374dd7d22aad8a6179f7ee1c948
赵春林 <946599275@qq.com>
2019-07-01 18:04:46 +0800

    Revert "修改cas锁，加入时间戳"
    This reverts commit 967ae1de

--- workspace-gateway/src/main/java/com/aliware/tianchi/util/GlobalConf.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/util/GlobalConf.java
@@ -14,6 +14,6 @@ public class GlobalConf {
     public static int[] smallMC = {109, 148, 108, 147, 121, 136, 101, 110, 116, 135};
     public static int[] mediumMC = {411, 392, 331, 365, 423, 370, 418, 379, 345, 355};
     public static int[] largeMC = {504, 484, 585, 512, 480, 518, 505, 535, 563, 534};
-    public static long LAST;
+    public static int PROVIDER;
     public static AtomicLong TIME;
 }
