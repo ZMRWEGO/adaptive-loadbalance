41dc74e0681fbe5d772d186ef4d8117a8ddf3cb8
赵春林 <946599275@qq.com>
2019-07-02 11:31:35 +0800

    动态调整权重

--- workspace-gateway/src/main/java/com/aliware/tianchi/util/GlobalConf.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/util/GlobalConf.java
@@ -11,9 +11,9 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class GlobalConf {
 
-    public static int[] smallMC = {109, 148, 108, 147, 121, 136, 101, 110, 116, 135};
-    public static int[] mediumMC = {411, 392, 331, 365, 423, 370, 418, 379, 345, 355};
-    public static int[] largeMC = {504, 484, 585, 512, 480, 518, 505, 535, 563, 534};
+    public static int[] smallMC = {109, 100, 108, 147, 121, 136, 101, 110, 116, 135};
+    public static int[] mediumMC = {411, 392, 331, 400, 370, 370, 418, 379, 345, 355};
+    public static int[] largeMC = {504, 484, 535, 512, 480, 518, 505, 500, 520, 500};
     public static int PROVIDER;
     public static AtomicLong TIME;
 }
