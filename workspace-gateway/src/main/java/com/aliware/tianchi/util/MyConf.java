beb29efc1b25576b68c5fe33d4d1031efa965e68
赵春林 <946599275@qq.com>
2019-07-03 14:18:47 +0800

    测试1

--- workspace-gateway/src/main/java/com/aliware/tianchi/util/MyConf.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/util/MyConf.java
@@ -12,9 +12,9 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class MyConf {
 
-    public static int[] smallMC = {150, 200, 200, 150};
-    public static int[] mediumMC = {500, 450, 500, 430};
-    public static int[] largeMC = {650, 650, 600, 630};
+    public static int[] smallMC = {150, 200, 200, 200};
+    public static int[] mediumMC = {500, 450, 500, 450};
+    public static int[] largeMC = {650, 650, 600, 650};
     public static AtomicBoolean smallException = new AtomicBoolean(false);
     public static AtomicBoolean mediumException = new AtomicBoolean(false);
     public static AtomicBoolean largeException = new AtomicBoolean(false);
