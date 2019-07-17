dd2fb04372300a9590ede766e02f85d86b980518
赵春林 <946599275@qq.com>
2019-07-17 13:35:48 +0800

    test123

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -30,20 +30,19 @@ import org.apache.dubbo.rpc.cluster.loadbalance.LeastActiveLoadBalance;
  * 负载均衡扩展接口 必选接口，核心接口 此类可以修改实现，不可以移动类或者修改包名 选手需要基于此类实现自己的负载均衡算法
  */
 public class UserLoadBalance implements LoadBalance {
-
-
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-        int small = GlobalConf.smallActive;
-        int medium = GlobalConf.mediumActive;
-        int large = GlobalConf.largeActive;
-        if (large <= Math.min(small, medium)) {
-            return invokers.get(2);
-        } else if (medium <= small) {
-            return invokers.get(1);
-        } else {
-            return invokers.get(0);
-        }
+//        int small = GlobalConf.smallActive;
+//        int medium = GlobalConf.mediumActive;
+//        int large = GlobalConf.largeActive;
+//        if (large <= Math.min(small, medium)) {
+//            return invokers.get(2);
+//        } else if (medium <= small) {
+//            return invokers.get(1);
+//        } else {
+//            return invokers.get(0);
+//        }
+        return invokers.get(randomOnWeight());
     }
 
     public  String stampToDate(long s){
@@ -71,5 +70,24 @@ public class UserLoadBalance implements LoadBalance {
         } else {
             return 4;
         }
+    }
+    private int randomOnWeight() {
+        int[] weightArray = new int[]{150, 500, 650};
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(150, 0);
+        map.put(500, 1);
+        map.put(650, 2);
+        int key = 0;
+        for (int weight : weightArray) {
+            treeMap.put(key, weight);
+            key += weight;
+        }
+
+        Random r = new Random();
+        int num;
+        num = r.nextInt(key);
+        return map.get(treeMap.floorEntry(num).getValue());
+
     }
 }
