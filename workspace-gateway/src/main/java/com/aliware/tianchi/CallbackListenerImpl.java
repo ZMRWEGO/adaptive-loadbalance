ab2b19dff9ebbb45dfc54305cce5f3b0d2f60a60
赵春林 <946599275@qq.com>
2019-07-20 11:34:18 +0800

    使用可用线程数分配

--- workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
@@ -11,28 +11,6 @@ public class CallbackListenerImpl implements CallbackListener {
 
     @Override
     public void receiveServerMsg(String msg) {
-        System.out.println("receive msg from server :" + msg);
-//        String[] s1 = msg.split(",");
-//        if (s1.length == 4) {
-//            String name = s1[3].split("=")[1];
-//            String exception = s1[2].split("=")[1];
-//            name = name.substring(0, name.length() - 1);
-//            exception = exception.substring(0, exception.length() - 1);
-//            switch (name) {
-//                case "small":
-//                    if (!exception.equals("null")) {
-//                        MyConf.smallException.set(true);
-//                    }
-//                case "large":
-//                    if (!exception.equals("null")) {
-//                        MyConf.largeException.set(true);
-//                    }
-//                case "medium":
-//                    if (!exception.equals("null")) {
-//                        MyConf.mediumException.set(true);
-//                    }
-//            }
-//        }
     }
 
 }
